3af1d8dbd6b1564cc8f10c7f1231fbe8d71e24e7
Bilal Muzaffer Kose <bilalmuzafferkose@gmail.com>
2023-01-18 16:42:57 +0300

    gitignore log , devtools, security exclude, applicationproperties

---
+++ src/main/java/com/bilalkose/utils/ConfigProgramming.java
@@ -0,0 +1,7 @@
+package com.bilalkose.utils;
+
+public class ConfigProgramming {
+    //DevTools => LiveReload
+    //file -> settings -> compiler -> Build project automatically
+    //settings -> advanced -> allow auto-make to start even
+}
