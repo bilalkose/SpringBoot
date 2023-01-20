81cd2a62b203315b12586d3dc1a853b37bd3e750
Bilal Muzaffer Kose <bilalmuzafferkose@gmail.com>
2023-01-20 16:01:16 +0300

    modelmapper pom.xml bug fix and first ModelMapperBean added and PasswordEncoderBean (inactive) added

---
+++ src/main/java/com/bilalkose/bean/PasswordEncoderBean.java
@@ -0,0 +1,16 @@
+package com.bilalkose.bean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+//@Configuration
+public class PasswordEncoderBean {
+
+//    @Bean
+//    public PasswordEncoder passwordEncoderBean(){
+//        //db ye şifreleri maskelenmiş olarak göndermek için
+//        return new BCryptPasswordEncoder();
+//    }
+}
