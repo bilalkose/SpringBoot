9f356aeb1dc5662688bffd387fb9d724bd7f9f52
Bilal Muzaffer Kose <bilalmuzafferkose@gmail.com>
2023-01-20 16:44:44 +0300

    Repository and ResourceNotFoundException first commit

---
+++ src/main/java/com/bilalkose/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.bilalkose.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+//JpaRepository>Crud Repository
+
+@Repository
+public interface EmployeeRepository extends CrudRepository {
+}
